e2cf88566cde287533ad39702d3a004c6a5f4db3
Nick Amin <namin@squark.physics.ucsb.edu>
2015-08-21 20:09:13 -0700

    bug fix

--- soundMaker/cmsSounds.py
+++ soundMaker/cmsSounds.py
@@ -1,12 +1,11 @@
 import json, urllib, os
 
 def playSound(filename):
-    # there might be flags for aplay we want to use later
+    # There might be flags for aplay we want to use later
     os.system("aplay %s" % filename)
 
 storage = "status.txt"
 url = "http://uaf-6.t2.ucsd.edu/~namin/monitoring/monitor.json"
-
 
 # Get previous status
 fh = open(storage,"r")
@@ -17,7 +16,7 @@ fh.close()
 
 # Get new status
 info = json.loads(urllib.urlopen(url).read())
-newStatus = info["status"].lower().strip()
+newStatus = info["status"].lower().strip().replace(" ","")
 beams = info["isgood"]["beams"] 
 bfield = info["isgood"]["bfield"] 
 systems = info["isgood"]["systems"]
@@ -26,18 +25,12 @@ fh = open(storage,"w")
 fh.write(newStatus + " " + str(int(beams)) + " " + str(int(bfield)) + " " + str(int(systems)))
 fh.close()
 
-# print wasTakingData, isTakingData
-# print prevStatus, newStatus
-
-# If status changed, play some sounds
-if newStatus != prevStatus:
-    if(isTakingData and not wasTakingData):
-        print ">>> Starting collecting data!"
-        playSound("takingdata.wav")
-    elif("dump" in newStatus):
-        print ">>> Beam dump!"
-        playSound("beamdump.wav")
-    else:
-        pass
-
+# Play some sounds
+if(isTakingData and not wasTakingData):
+    print ">>> Starting collecting data!"
+    playSound("takingdata.wav")
+elif("dump" in newStatus and "dump" not in prevStatus):
+    print ">>> Beam dump!"
+    playSound("beamdump.wav")
+else: pass
 
