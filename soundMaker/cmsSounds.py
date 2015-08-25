c96dd0f2368ce52cc2131e94e830993e8c29c4ba
Bennett Marsh <bmarsh@physics.ucsb.edu>
2015-08-25 10:54:51 -0700

    fixed non-detect of beam dump

--- soundMaker/cmsSounds.py
+++ soundMaker/cmsSounds.py
@@ -8,30 +8,35 @@ def playSound(filename):
 sounds = True
 storage = "status.txt"
 url = "http://uaf-6.t2.ucsd.edu/~namin/monitoring/monitor.json"
+#url = "../cmsMonitorMaker/monitor.json"
+
 
 # Get previous status
 fh = open(storage,"r")
 info = fh.read().lower().strip()
 prevStatus = info.split()[0]
-wasTakingData = sum(map(int, info.split()[1:])) == 3
+prevLastDump = info.split()[1]
+wasTakingData = sum(map(int, info.split()[2:])) == 3
 fh.close()
 
 # Get new status
 info = json.loads(urllib.urlopen(url).read())
+#info = json.loads(open(url).read())
 newStatus = info["status"].lower().strip().replace(" ","")
 beams = info["isgood"]["beams"] 
 bfield = info["isgood"]["bfield"] 
 systems = info["isgood"]["systems"]
+newLastDump = info["lastbeamdump"]
 isTakingData = beams and bfield and systems
 fh = open(storage,"w")
-fh.write(newStatus + " " + str(int(beams)) + " " + str(int(bfield)) + " " + str(int(systems)))
+fh.write(newStatus + " " + newLastDump + " " + str(int(beams)) + " " + str(int(bfield)) + " " + str(int(systems)))
 fh.close()
 
 # Play some sounds
 if(isTakingData and not wasTakingData):
     print ">>> Starting collecting data!"
     if(sounds): playSound("datacollection.wav")
-elif("dump" in newStatus and "dump" not in prevStatus):
+elif prevLastDump != newLastDump:
     print ">>> Beam dump!"
     if(sounds): playSound("beamdump_withvoice.wav")
 else: pass
