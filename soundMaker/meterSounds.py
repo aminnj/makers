9a57b162d2a788252366f7619be9be9dbe49f22f
Nick Amin <namin@squark.physics.ucsb.edu>
2015-08-28 17:10:32 -0700

    add metersounds

---
+++ soundMaker/meterSounds.py
@@ -0,0 +1,24 @@
+import json, urllib, os
+
+def sayText(text):
+    # print text
+    os.system('espeak -v mb-en1 "%s"' % text)
+
+storage = "meter.txt"
+url = "http://uaf-6.t2.ucsd.edu/~namin/makers/meterMaker/data.txt"
+
+fh = open(storage,"r")
+oldinfo = json.loads(fh.read().strip())
+newinfo = json.loads(urllib.urlopen(url).read())
+fh = open(storage,"w")
+fh.write(json.dumps(newinfo))
+fh.close()
+
+for i,meter in enumerate(oldinfo["meters"]):
+    oldfrac = float(oldinfo["meters"][i]["pointer"]) 
+    newfrac = float(newinfo["meters"][i]["pointer"])
+    metertitle = oldinfo["meters"][i]["title"]
+    if(abs(oldfrac-newfrac) > 0.05):
+        sayText("%s meter was changed from %i percent to %i percent" % (metertitle, 100*oldfrac, 100*newfrac))
+        break
+
---
+++ soundMaker/sounds.py
@@ -0,0 +1,3 @@
+import cmsSounds, meterSounds
+
+### 
