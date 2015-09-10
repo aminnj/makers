04b24f74da7d450b4e358e890a6e54e9a3a1e3c4
Nick Amin <namin@squark.physics.ucsb.edu>
2015-09-09 18:06:28 -0700

    revamp

--- moneyMaker/scripts/downloadAll.py
+++ moneyMaker/scripts/downloadAll.py
@@ -7,7 +7,7 @@ for line in csv.split("\n"):
     try:
         stock = line.split("|")[0]
         # print stock
-        gs.saveStock(stock,2005,2015)
+        gs.saveStock(stock,(2005,1,1),(2015,12,31))
     except:
         continue
 
--- moneyMaker/scripts/getStocks.py
+++ moneyMaker/scripts/getStocks.py
@@ -1,44 +1,68 @@
 import urllib2, time, datetime
 import pickle, gzip
+from matplotlib.dates import date2num, num2date
+from matplotlib.cbook import iterable
+
+# conventions: d1, d2 are 3-tuples of year,month,day
+#              dt1, dt2, are datetime objects
+#              day1, day2, are ints, given by #days+1 since 01-01-01
+# => http://matplotlib.org/api/dates_api.html#matplotlib.dates.date2num
 
 storageDir = "../data/"
 
-def saveStock(stock, yearStart, yearEnd):
-    pickleFile = "%s/%s.pklz" % (storageDir, stock)
+def date2inum(dt): return int(date2num(dt))
+
+def saveStock(ticker, d1, d2):
+    ticker = ticker.upper()
+    pickleFile = "%s/%s.pklz" % (storageDir, ticker)
+
+
+    if iterable(d1): d1 = (d1[0], d1[1], d1[2])
+    else: d1 = (d1.year, d1.month, d1.day)
+
+    if iterable(d2): d2 = (d2[0], d2[1], d2[2])
+    else: d2 = (d2.year, d2.month, d2.day)
 
     dOld = { }
     alreadyExists = False
 
-    # try to open an already existing pickle file for this stock
+    # try to open an already existing pickle file for this ticker
     try:
         fh = gzip.open(pickleFile, "rb")
         dOld = pickle.load(fh)
         fh.close()
         alreadyExists = True
     except:
-        print "[MM] %s: No existing data file. Making one now." % stock
+        print "[MM] %s: No existing data file. Making one now." % ticker
 
     # check to see if the requested dates are already within datafile, to reduce API calls
     if(alreadyExists):
-        # move firstDay back 4 days and lastDay up 4 days to account for weekends and holidays
-        firstDay = datetime.date(yearStart,1,1)+datetime.timedelta(days=3)
-        lastDay = datetime.date(yearEnd,12,31)-datetime.timedelta(days=3)
-        timeFirstDay = time.mktime(firstDay.timetuple())
-        timeLastDay = time.mktime(lastDay.timetuple())
+        # monday is 0, friday is 4
+        dt1, dt2 = datetime.datetime(*d1), datetime.datetime(*d2)
+        # print dt1, dt2, dt1.weekday(), dt2.weekday()
+        # move d1 up to be monday
+        if(dt1.weekday() > 4): dt1 += datetime.timedelta(days=7-dt1.weekday())
+        if(dt2.weekday() > 4): dt2 -= datetime.timedelta(days=dt2.weekday()-4)
 
-        if ( (timeFirstDay > dOld["firstTime"]) and (timeLastDay < dOld["lastTime"]) ):
-            print "[MM] %s: Already saved specified time period (%i-%i)." % (stock, yearStart, yearEnd)
+        
+        d1new, d2new = date2inum(dt1), date2inum(dt2)
+
+        # print dt1, dt2, dt1.weekday(), dt2.weekday()
+        # print d1new, d2new, dOld["day1"], dOld["day2"]
+        # print num2date(dOld["day1"])
+        # print num2date(dOld["day1"]).weekday()
+
+        if ( (d1new >= dOld["day1"]) and (d2new <= dOld["day2"]) ):
+            print "[MM] %s: Already saved specified time period [(%i,%i,%i)-(%i,%i,%i)]." % (ticker, d1[0],d1[1],d1[2], d2[0],d2[1],d2[2])
             return
 
+    urlFmt = 'http://ichart.yahoo.com/table.csv?a=%d&b=%d&c=%d&d=%d&e=%d&f=%d&s=%s&y=0&g=%s&ignore=.csv'
+    url = urlFmt % (d1[1]-1, d1[2], d1[0], d2[1]-1, d2[2], d2[0], ticker, 'd')
 
-
-
-    # start on Jan 1st of yearStart, end on Dec 31st of yearEnd
-    url="http://ichart.yahoo.com/table.csv?s=%s&a=0&b=1&c=%i&d=11&e=31&f=%i&g=d&ignore=.csv" % (stock,yearStart,yearEnd)
     try:
         csv = urllib2.urlopen(url).read()
     except:
-        print "[MM] %s: No data available for part of specified time period (%i-%i)." % (stock, yearStart, yearEnd)
+        print "[MM] %s: No data available for part of specified time period [(%i,%i,%i)-(%i,%i,%i)]." % (ticker, d1[0],d1[1],d1[2], d2[0],d2[1],d2[2])
         return
 
     lines = csv.split()[2:] # first 2 lines are just the column names 
@@ -46,52 +70,74 @@ def saveStock(stock, yearStart, yearEnd):
 
     d = {}
 
-    d["stock"] = stock
-    d["yearStart"] = yearStart
-    d["yearEnd"] = yearEnd
-
+    d["ticker"] = ticker
+    minday, maxday = 9e7, 0
     for i,line in enumerate(lines):
         line = line.strip()
-        # print line
-        date, openPrice, highPrice, lowPrice, closePrice, volume, adjustedClosePrice = line.split(",")
-        openPrice = round(float(openPrice),2)
-        closePrice = round(float(closePrice),2)
-        highPrice = round(float(highPrice),2)
-        lowPrice = round(float(lowPrice),2)
-        adjustedClosePrice = round(float(adjustedClosePrice),2)
+        dateString, openPrice, highPrice, lowPrice, closePrice, volume, adjClosePrice = line.split(",")
+        openPrice,closePrice,highPrice,lowPrice,adjustedClosePrice = map(lambda x: round(float(x),2), (openPrice,closePrice,highPrice,lowPrice,adjClosePrice))
         volume = int(volume)
-        date = time.strptime(date,"%Y-%m-%d") # looks like 2001-01-17
-        unixtime = int(time.mktime(date))
-        d[unixtime] = { 
+        date = datetime.datetime.strptime(dateString,"%Y-%m-%d") # looks like 2001-01-17
+        day = date2inum(date)
+        
+        d[day] = { 
                 "open": openPrice,
                 "high": highPrice,
                 "low": lowPrice,
                 "close": closePrice,
-                "adjustedclose": adjustedClosePrice,
+                "adjustedclose": adjClosePrice,
                 "volume": volume
                 }
 
         # save first and last times for this API call for range checking later
-        if(i == numDays-1): d["firstTime"] = unixtime
-        if(i == 0): d["lastTime"] = unixtime
-        
+        if(day <= minday):
+            minday = day
+            d["day1"] = day
+            d["d1"] = date.timetuple()
+        if(day >= maxday):
+            maxday = day
+            d["day2"] = day
+            d["d2"] = date.timetuple()
 
-
-    # if data already exists, then add the new dates to it
+    # if data already exists, then add only the new dates to it
     if(alreadyExists):
-        for date in d.keys():
-            if( not( dOld["firstTime"] <= date <= dOld["lastTime"] ) ):
-                dOld[date] = d[date]
+        for day in d.keys():
+            if ( not (date2inum(datetime.datetime(*d1)) <= day <= date2inum(datetime.datetime(*d2))) ): dOld[day] = d[day]
+
+        # stretch out the ranges when adding the new dates, if needed
+        if(dOld["day1"] <= d["day1"]):
+            d["day1"] = dOld["day1"]
+            d["d1"] = dOld["d1"]
+        if(dOld["day2"] <= d["day2"]):
+            d["day2"] = dOld["day2"]
+            d["d2"] = dOld["d2"]
     else: dOld = d
 
     fh = gzip.open(pickleFile, "wb")
     pickle.dump(dOld, fh)
     fh.close()
 
-    print "[MM] %s: Successfully saved data for time period (%i-%i)." % (stock, yearStart, yearEnd)
+    d1, d2 = d["d1"], d["d2"]
+    print "[MM] %s: Successfully saved data for time period [(%i,%i,%i)-(%i,%i,%i)]." % (ticker, d1[0],d1[1],d1[2], d2[0],d2[1],d2[2])
 
+def getStock(ticker, d1, d2):
+    saveStock(ticker, d1, d2)
 
+    pickleFile = "%s/%s.pklz" % (storageDir, ticker)
 
-# saveStock("AAPL", 2001, 2015)
-# saveStock("AAPL", 2002, 2004)
+    fh = gzip.open(pickleFile, "rb")
+    dOld = pickle.load(fh)
+    fh.close()
+
+    day1, day2 = dOld["day1"], dOld["day2"]
+
+    d = { }
+    for day in dOld.keys():
+        if( day1 <= day <= day2 ):
+            d[day] = dOld[day]
+
+    return d
+
+# saveStock("INTC", (2005,1,11), (2015,2,17))
+# getStock("INTC", (2006,1,8), (2006,2,8))
 
