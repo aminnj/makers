3b1522a21ee181b8d6e7056f5582c5df0a75b07a
Nick Amin <amin.nj@gmail.com>
2015-09-16 16:51:17 -0700

    more cluster stuff...compute profit after different cluster types

--- moneyMaker/scripts/avg.py
+++ moneyMaker/scripts/avg.py
@@ -10,70 +10,51 @@ import cluster as cl
 
 plotdir = "../plots/"
 
-
-# stock = gs.getStock("WMT", (2009, 2, 15), (2014, 5, 3))["days"] # for calculating
-# d1,d2 = (2013,8,9),(2014,4,25) # for plotting
-
-stock = gs.getStock("MCD", (2006, 1, 1), (2008, 10, 5))["days"] # for calculating
+stock = gs.getStock("F", (2005, 1, 1), (2014, 10, 5))["days"] # for calculating
 d1,d2 = (2007,8,1),(2008,3,5) # for plotting
 
 quotes = []
-normquotes = []
 timesclose = []
 for day in sorted(stock.keys()):
     vals = stock[day]
     o, h, l, c = vals["o"], vals["h"], vals["l"], vals["c"]
 
-    # "normalize" values (subtract out open price)
-    no = round(o-o,2)
-    nh = round(h-o,2)
-    nl = round(l-o,2)
-    nc = round(c-o,2)
-
     quotes.append( [day,o,h,l,c] )
-    normquotes.append( [day,no,nh,nl,nc] )
     timesclose.append([day,c])
 
 timesclose = np.array(timesclose)
 bbands = ind.bbtimes(timesclose, 20)
 
-emas = []
-for i in [1,3,4,8]:
-    emas.append( ind.ematimes(timesclose,10*i) )
+emas = [ ind.ematimes(timesclose,i) for i in [5,10] ]
 
 quotes = np.array(quotes)
 rsis = ind.rsitimes(quotes[:,[0,1]],14)
 
 # bbands discards early dates since it can't do a moving average for those
 # we reshape the other arrays to match up with bbands' size
-normquotes = np.array(normquotes[-len(bbands):])
 quotes = quotes[-len(bbands):]
 
-nclusters=7
-ncandles=5
-# normquotes = []
-# for quotes in normquotes:
 
-times, clusters, clusterCenters = cl.clusterCandles(normquotes, nclusters=nclusters, ncandles=ncandles)
+nclusters=10
+ncandles=10 # remember to change to "saveTo" below when you change these values
+times, clusters, clusterCenters = cl.clusterCandles(quotes, nclusters=nclusters, ncandles=ncandles, loadFrom="test.txt")
+clusterQuotes, vlines = cl.clusterCentersForPlotting(clusterCenters)
 
-# print clusterCenters
-clusterCenters = clusterCenters.reshape(nclusters*ncandles,3)
+print cl.clustersAndTheirProfits(clusters,clusterCenters)
 
-colors = list("rbgkcmyw") # only allows up to 8
-clustershading = []
-for i, day in enumerate(times):
-    clustershading.append([ day, colors[clusters[i]] ])
+
+# colors = list("rbgkcmyw") # only allows up to 8
+# clustershading = []
+# for i, day in enumerate(times):
+#     clustershading.append([ day, colors[clusters[i]] ])
 
 crossovershading = []
 for day, rising in ind.crossovertimes(emas):
     crossovershading.append([day, 'g' if rising else 'r'])
 
-clusterQuotes = np.c_[ np.array(range(1000,1000+len(clusterCenters))), np.zeros(len(clusterCenters)), clusterCenters ]
-print clusterQuotes[:,0]
-
-vlines = range(1000,1000+len(clusterQuotes[:,0]),ncandles)
 
 u.makeCandlestick(clusterQuotes, "../plots/candlestick.png",title="cluster centers",vlines=vlines)
+
 # u.makeCandlestick(quotes, "../plots/candlestick.png", title="WMT (2009)", \
 #         shadings=[clustershading,crossovershading], \
 #         bbands=bbands, \
@@ -81,4 +62,5 @@ u.makeCandlestick(clusterQuotes, "../plots/candlestick.png",title="cluster cente
 #         averages=emas, \
 #         rsis=rsis, \
 #         )
+
 # u.web("../plots/candlestick.png")
--- moneyMaker/scripts/cluster.py
+++ moneyMaker/scripts/cluster.py
@@ -1,36 +1,111 @@
 import numpy as np
 import sklearn.cluster as skc
 
-def clusterCandles(normquotes, nclusters=5, ncandles=2, shift=0):
+# def clusterCandles(normquotes, nclusters=5, ncandles=2, shift=0):
+def clusterCandles(quotes, nclusters=5, ncandles=2, loadFrom=None, saveTo=None, improve=False):
     """
-    - takes normalizedquotes (e.g., subtract open price from everything), which is
-    a list of [day,o,h,l,c] vals. shift is the offset.
-    - returns a 1D array of start times for candles and a 1D array of cluster numbers
+    - takes quotes object
+    - returns:
+      a 1D array of start times for candles 
+      a 1D array of cluster numbers
+      an array of cluster centers (array of vectors)
+    - specify filename in loadFrom to load initial centroids
+    - specify saveTo to save centroids
+    - specify improve=True when you have loadFrom to load centroids, and train further
     """
-    # each candle is described by 3 values
-    forfit = normquotes[:,[2,3,4]][shift:] # no need for low because we subtracted that out...always 0
-    times = normquotes[:,0][shift:]
 
-    forfit = forfit[len(forfit)%ncandles:] # drop excess candles so we have a multiple of ncandles
-    times = times[len(times)%ncandles:]
-
+    # remove the first few elements so that the size is a multiple of ncandles; ignore dates
+    forfit = quotes[:,[1,2,3,4]][len(quotes)%ncandles:] 
+    times = quotes[:,0][len(quotes)%ncandles:] 
     forfit = forfit.ravel() # flattens n-dim to 1-dim
+    # group candles together in groups of size ncandles
+    # turn it into [ [o h l c o h l c ... ] # 4*ncandles elements
+    #                [o h l c o h l c ... ]  ... ]
+    rows, cols = int(np.ceil(len(forfit)/(ncandles*4))),4*ncandles
+    forfit.resize(rows, cols)
     times = times.ravel()
-    rows, cols = int(np.ceil(len(forfit)/(ncandles*3))),3*ncandles # rows is num of candle clumps
-    forfit.resize(int(np.ceil(len(forfit)/(ncandles*3))),3*ncandles)
-    times.resize(rows,cols//3)
+    times.resize(rows,cols//4)
     times = times[:,0]
+    # now subtract the first open price from all elements (within each group)
+    # and normalize so that highest-lowest=1
+    normforfit = []
+    for group in forfit: 
+        maxp, minp = np.max(group), np.min(group)
+        normforfit.append( (group - group[0])/(maxp - minp) )
+    normforfit = np.array(normforfit)
+    
+    if(loadFrom is not None):
+        if(not improve): km = skc.KMeans(n_clusters=nclusters,max_iter=1,init=np.loadtxt(loadFrom))
+        else: km = skc.KMeans(n_clusters=nclusters,max_iter=600,init=np.loadtxt(loadFrom))
+    else:
+        km = skc.KMeans(n_clusters=nclusters,max_iter=600,n_init=20)
 
-    km = skc.KMeans(n_clusters=nclusters)
-
-    # add more and more columns to data (c_ just puts columns side by side)
-    # data = np.c_[ normquotes[:,[2,3,4]] ] # no need for low because we subtracted that out...always 0
-    data = np.c_[ forfit ] # no need for low because we subtracted that out...always 0
-    # data = np.c_[ data, bbands[:,1]-quotes[:,1] ] # subtract out opening prices from bbands
-    # data = np.c_[ data, bbands[:,2]-quotes[:,1] ]
-    # data = np.c_[ data, bbands[:,3]-quotes[:,1] ]
-
+    data = np.c_[ normforfit ]
     clusters = km.fit_predict(data)
     clusterCenters = km.cluster_centers_
 
+    if(saveTo is not None):
+        np.savetxt(saveTo,clusterCenters)
+
     return times, clusters, clusterCenters
+
+def clusterCentersForPlotting(clusterCenters):
+    """
+    - takes array of cluster centers (from above function)
+    - returns quotes object and vlines object to directly feed into candlestick plotting function
+    - dates don't matter (they're just dummy values) as this is only for visualization of cluster geometry
+    """
+    clusterCenters = np.c_[ clusterCenters ]
+    nclusters, ncandles = len(clusterCenters), len(clusterCenters[0])//4
+    clusterCenters = clusterCenters.reshape(nclusters*ncandles,4)
+    clusterQuotes = np.c_[ np.array(range(1000,1000+len(clusterCenters))), clusterCenters ]
+    vlines = range(1000,1000+len(clusterQuotes[:,0]),ncandles)
+
+    return clusterQuotes, vlines
+
+def dictToSortedList(d, reverse=True):
+    """
+    takes a dict with key,vals
+    returns an array with elements [key,val] sorted with descending value
+    """
+    sortedkeys = sorted(d, key=d.get, reverse=reverse)
+    sortedvals = [d[k] for k in sortedkeys]
+    return np.array(zip(sortedkeys, sortedvals))
+
+def profitFromCluster(cluster):
+    # returns NORMALIZED profit for a given cluster vector
+    # want close of last day - open of first day
+    # since it is O h l c o h l C
+    return cluster[-1]-cluster[0]
+
+def clustersAndTheirProfits(clusters, clusterCenters):
+    # give me an array of clusters codes (1D) and an array of clusterCenters
+    # I will give you a dict of clusters and their normalized profits
+    # due to a weighted average of all possible following clusters
+
+    # want to find which cluster follows another cluster most of the time
+    clusterpairs = zip(clusters[:-1], clusters[1:]) 
+    # make histogram dict
+    dpairs = {}
+    for one,two in clusterpairs:
+        if one not in dpairs: dpairs[one] = { }
+        if two not in dpairs[one]: dpairs[one][two] = 0
+
+        dpairs[one][two] += 1
+
+    clusterProfits = {}
+    for one in sorted(dpairs.keys()):
+        twos = dictToSortedList(dpairs[one])
+        if(len(twos) > 0): tot = np.sum(twos[:,1])
+        else: tot = 1
+        # for each cluster pattern, figure out the cluster patterns that follow
+        s = 0.0
+        for two in twos:
+            # sum up the profit from the following cluster pattern weighted by it's probability
+            # to follow the initial cluster pattern
+            frac = 1.0*two[1]/tot
+            s += frac*profitFromCluster(clusterCenters[two[0]])
+        clusterProfits[one] = s
+
+    return clusterProfits
+
