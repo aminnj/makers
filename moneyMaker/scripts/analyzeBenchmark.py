d872978ed159e780f61b3a9edd7128f09bd012f3
jgran <jgran@physics.ucsb.edu>
2015-09-15 23:43:01 -0700

    add win percentage to backtest reporting

--- moneyMaker/scripts/analyzeBenchmark.py
+++ moneyMaker/scripts/analyzeBenchmark.py
@@ -40,6 +40,7 @@ print "RAND:"
 print "rand strat yields $(%.2f +- %.2f)/day" % (np.mean(rands[:,0]/days), np.mean(rands[:,1]/days)/np.sqrt(100))
 print "rand strat yields $(%.2f +- %.2f)/trade" % (np.mean(rands[:,0]/rands[:,2]), np.mean(rands[:,1]/rands[:,2])/np.sqrt(100))
 print "rand strat has %.1f trades/yr" % (365.0*np.mean(rands[:,2]/days))
+print "rand strat win percentage: %i" % (np.sum(rands[:,2]*rands[:,3])/np.sum(rands[:,2]))
 print "-"*30,"\n"
 
 # bah
@@ -47,6 +48,7 @@ print "BAH:"
 print "bah strat yields $%.2f/day" % np.mean(bahs[:,0]/days)
 print "bah strat yields $%.2f/trade" % np.mean(bahs[:,0]/bahs[:,2])
 print "bah strat has %.1f trades/yr" % (365.0*np.mean(bahs[:,2]/days))
+print "bah strat win percentage: %i" % (np.sum(bahs[:,2]*bahs[:,3])/np.sum(bahs[:,2]))
 print "-"*30,"\n"
 
 # user
@@ -54,6 +56,7 @@ print "USER:"
 print "user strat yields $%.2f/day" % np.mean(users[:,0]/days)
 print "user strat yields $%.2f/trade" % np.mean(users[:,0]/users[:,2])
 print "user strat has %.1f trades/yr" % (365.0*np.mean(users[:,2]/days))
+print "user strat win percentage: %i" % (np.sum(users[:,2]*users[:,3])/np.sum(users[:,2]))
 print "-"*30,"\n"
 
 sigmas = (users[:,0]-rands[:,0])/rands[:,1] # how many sigma away is user from rand
