74b794213e39c0378c8b7bb43bc4d86db8971c0b
Nick Amin <amin.nj@gmail.com>
2015-10-15 23:41:50 -0700

    uaf support

--- jsonMaker/viewer.py
+++ jsonMaker/viewer.py
@@ -19,15 +19,22 @@ if jsonName:
     filename = jsonDir+shortName
 
     # download and save json if it doesn't exist
-    if not os.path.isfile(filename):
-        url = "http://namin.web.cern.ch/namin/getJSON.php?json=%s" % (jsonName)
-        output = urllib2.urlopen(url).read()
-        if(len(output) > 10):
-            fh = open(filename, "w")
-            fh.write(output)
-            fh.close()
+    if("/afs/" in jsonName):
+        if not os.path.isfile(filename):
 
-
+            # download from lxplus
+            url = "http://namin.web.cern.ch/namin/getJSON.php?json=%s" % (jsonName)
+            output = urllib2.urlopen(url).read()
+            if(len(output) > 10):
+                fh = open(filename, "w")
+                fh.write(output)
+                fh.close()
+    else:
+        # copy from uaf
+        os.system("cp %s %s/%s" % (jsonName, jsonDir, shortName))
+        pass
+        # /home/users/namin/sandbox/missingLumis/json.txt
+            
 
     fh = open(filename, "r")
     data = fh.read()
@@ -54,8 +61,9 @@ else:
     print "Content-type:text/html\r\n"
     print "<html>"
     print "<body>"
-    print "Um, try putting something like"
+    print "Hey, try putting something like"
     print "<pre>    ?json=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-257599_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt</pre>"
-    print "at the end of the url"
+    print "at the end of the url to fetch from lxplus, or you can even look at the UAF"
+    print "<pre>    ?json=/home/users/namin/sandbox/missingLumis/json.txt</pre>"
     print "</body>"
     print "</html>"
