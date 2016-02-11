dbf0f57a1a921fa8b5df00b30cb5694223a9e1fb
Nick Amin <amin.nj@gmail.com>
2016-02-10 23:08:12 -0800

    change format

--- cmsMonitorMaker/add_uaf_health.py
+++ cmsMonitorMaker/add_uaf_health.py
@@ -7,7 +7,7 @@ stat,out = commands.getstatusoutput("awk 'BEGIN {ORS=\" \"}; /^uaf-/{print; getl
 times = {int(line.split(".")[0].split("-")[1]):float(line.split("real")[1]) for line in out.split("\n") if len(line)>1}
 c1 = np.array([0,212,0]) # green
 c2 = np.array([236,13,13]) # red
-buff="(as of %s): &nbsp;" % str(datetime.datetime.now().strftime("%H:%M"))
+buff="(as of %s): &nbsp;" % str(datetime.datetime.now().strftime("%I:%M %p"))
 for uaf in range(3,11):
     f = 1.0 # fraction of the way from c1 to c2
     if uaf in times: f = times[uaf]/15.0
