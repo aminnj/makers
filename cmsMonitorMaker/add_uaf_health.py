afee9f1f6828a333f5b2295a35a6f28e03cab000
Nick Amin <amin.nj@gmail.com>
2016-02-10 23:05:42 -0800

    add timestamp

--- cmsMonitorMaker/add_uaf_health.py
+++ cmsMonitorMaker/add_uaf_health.py
@@ -1,12 +1,13 @@
 import numpy as np
 import commands
+import datetime
 
 stat,out = commands.getstatusoutput("for i in {3..10}; do time -p (ssh -o ConnectTimeout=15 -o BatchMode=yes -o StrictHostKeyChecking=no namin@uaf-${i}.t2.ucsd.edu hostname); done >& tempuptime.txt")
 stat,out = commands.getstatusoutput("awk 'BEGIN {ORS=\" \"}; /^uaf-/{print; getline; print; print \"\\n\"}' tempuptime.txt")
 times = {int(line.split(".")[0].split("-")[1]):float(line.split("real")[1]) for line in out.split("\n") if len(line)>1}
 c1 = np.array([0,212,0]) # green
 c2 = np.array([236,13,13]) # red
-buff=""
+buff="(as of %s): &nbsp;" % str(datetime.datetime.now().strftime("%H:%M"))
 for uaf in range(3,11):
     f = 1.0 # fraction of the way from c1 to c2
     if uaf in times: f = times[uaf]/15.0
