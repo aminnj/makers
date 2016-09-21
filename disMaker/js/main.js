09747ee243a88b132da06063e1a77699765d39e2
Nick Amin <amin.nj@gmail.com>
2016-09-20 17:55:48 -0700

    clean up and remove the box that shows the user's query as this was kind of pointless

--- disMaker/js/main.js
+++ disMaker/js/main.js
@@ -7,10 +7,21 @@ function prettyJSON(elem, json) {
 }
 
 var t0;
+function handleResponse(response) {
+    console.log(response);
+    if(response["response"]["status"] == "success" && response["response"]["warning"].length == 0) {
+        prettyJSON($("#result"), response["response"]["payload"]);
+    } else {
+        prettyJSON($("#result"), response["response"]);
+    }
+    $("#loading_animation").hide();
+    $("#result_container").show();
+    $(".timing").html("loaded in " + (new Date().getTime()-t0)/1000 + " seconds");
+}
 function doSubmit(data) {
     $("#loading_animation").show();
-    $("#query_container").show();
-    prettyJSON($('#query'), data);
+    // $("#query_container").show();
+    // prettyJSON($('#query'), data);
     var cli_str = "dis_client.py ";
     if(data["type"] != "basic") cli_str += "-t " + data["type"] + " ";
     if(data["short"] != "short") cli_str += "--detail ";
@@ -20,34 +31,11 @@ function doSubmit(data) {
     t0 = new Date().getTime();
     $.get("handler.py", data)
         .done(function(response) {})
-        .always(function(response){
-            console.log(response);
-            if(response["response"]["status"] == "success" && response["response"]["warning"].length == 0) {
-                prettyJSON($("#result"), response["response"]["payload"]);
-            } else {
-                prettyJSON($("#result"), response["response"]);
-            }
-            $("#loading_animation").hide();
-            $("#result_container").show();
-            $(".timing").html("loaded in " + (new Date().getTime()-t0)/1000 + " seconds");
-    });
-}
-
-var examplesVisible = false;
-function toggleExamples() {
-    $("#examples_container").slideToggle();
-    if(examplesVisible) {
-        $("#toggle_examples").text("show examples");
-        examplesVisible = false;
-    } else {
-        $("#toggle_examples").text("hide examples");
-        examplesVisible = true;
-    }
-
+        .always(handleResponse);
 }
 
 function initHide() {
-    $("#query_container").hide();
+    // $("#query_container").hide();
     $("#result_container").hide();
     $("#loading_animation").hide();
 }
